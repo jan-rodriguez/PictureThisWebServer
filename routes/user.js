847f5a1e1b1f4d951a0b7278c79a2616f3b113ed
jkr241 <jkr241@mit.edu>
2015-02-22 21:31:50 -0500

    Trying to get database to work

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,41 @@ USER ROUTES
 */
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
 
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
+router.get('/:id(\\d+)', function(req, res, next) {
   res.json({hello: "world"});
 });
 
 /* GET all users */
 router.get('/all', function(req, res, next) {
   res.json({hi: "there"});
+});
+
+/* POST create user */
+router.get('/new', function(req, res, next) {
+  res.send("hello");
+  var conn = mysql.createConnection({
+    host    : "localhost",
+    user    : "root",
+    password: "root",
+    port    : 3306,
+    database: "ops_db"
+
+  });
+
+  conn.connect(function(err) {
+    if(err) {
+      console.error("CONNECTION IS FAILING:" + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  console.log("Hey bb");
+  conn.end()
 });
 
 module.exports = router;
