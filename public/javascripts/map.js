2bf0d21fbe796182604b1692d119955a0dc0ce60
jkr241 <jkr241@mit.edu>
2015-03-09 06:04:26 -0400

    Added a hot and cold marker, can't use because it is not precise enough

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -12,51 +12,56 @@ $(document).ready(function(){
   function initializeMap(location) {
     var mapCanvas = document.getElementById('map-canvas');
 
+    //NOISE VARIANCE, IN DEGREES
+    //IS STILL WITHIN THE CIRCLE BECAUSE MAX DISTANCE OF .002 DEGREES IS 222 METERS AT EQUATOR
     var noiseMax = .002;
-    var circleRadius = 400;
 
+    //Actual latitude and longitude values
     var latitude = location.latitude;
     var longitude = location.longitude;
 
     //Add randomness to location, so it's not always in the middle
     var randomNoise = Math.random() * noiseMax;
-    latitude = Math.random() > .5 ? latitude + randomNoise : latitude - randomNoise;
+    var fakeLatitude = Math.random() > .5 ? latitude + randomNoise : latitude - randomNoise;
     randomNoise = Math.random() * noiseMax;
-    longitude = Math.random() > .5 ? longitude + randomNoise : longitude - randomNoise;
+    var fakeLongitude = Math.random() > .5 ? longitude + randomNoise : longitude - randomNoise;
 
 
     var mapOptions = {
-      center: new google.maps.LatLng(latitude, longitude),
+      center: new google.maps.LatLng(fakeLatitude, fakeLongitude),
       zoom: 16,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     var map = new google.maps.Map(mapCanvas, mapOptions);
 
+    //In meters, HAS TO AT LEAST BE LARGER THAN 222 TO BE WITHIN NOISE
+    var hintCircleRadius = 400;
     // Construct the polygon.
-    hintCircle = {
+    var hintCircleOptions = {
       strokeColor: '#00AA00',
       strokeOpacity: 0.8,
       strokeWeight: 2,
       fillColor: '#00AA00',
-      fillOpacity: 0.15,
+      fillOpacity: 0.1,
       map: map,
-      center: new google.maps.LatLng(latitude, longitude),
-      radius: circleRadius
+      center: new google.maps.LatLng(fakeLatitude, fakeLongitude),
+      radius: hintCircleRadius
     };
 
     //Draw circle
-    var newCircle = new google.maps.Circle(hintCircle);
-
+    var hintCircle = new google.maps.Circle(hintCircleOptions);
 
     var GeoMarker = new GeolocationMarker(map);
 
+    //Hide the accuracy marker
     GeoMarker.setCircleOptions({
       strokeColor: '#cccccc',
-      strokeOpacity: 0.8,
+      strokeOpacity: .8,
       strokeWeight: 2,
       fillColor: '#cccccc',
-      fillOpacity: 0.5
+      fillOpacity: .2,
+      visible: false
     });
 
     //Error handling
@@ -71,6 +76,80 @@ $(document).ready(function(){
         alert("Error getting location.")
       }
     });
+
+    var hotOrColdCircleOptions = {
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.25,
+      map: map,
+      radius: hintCircleRadius/15,
+      visible: false
+    };
+    //Cicle showing the player how close they are to the actual location
+    var hotOrColdCircle = new google.maps.Circle(hotOrColdCircleOptions);
+
+    //Update the hint circle to match the current location
+    GeoMarker.addListener('position_changed', function(){
+
+      var currentPos = GeoMarker.getPosition();
+
+      //Set center of hot or cold circle at player's location
+      hotOrColdCircle.setCenter(currentPos);
+
+      var newColor = getHorOrColdColor(currentPos);
+
+      hotOrColdCircle.setOptions({
+        color: newColor,
+        visible: true
+      });
+
+    });
+
+    function getHorOrColdColor(latLong){
+      var curLat = latLong.lat();
+      var curLong = latLong.lng();
+
+      console.log("Lat, long: "+latitude+", "+longitude);
+      console.log("Cur lat, long: "+curLat+", "+curLong);
+
+      //TODO: TRY TO MAKE THIS METERS, NOT USING LATITUDE AND LONGITUDE
+      //CHECKOUT http://www.movable-type.co.uk/scripts/latlong.html
+      var distance = Math.abs(Math.sqrt((latitude*latitude) + (longitude*longitude)) - Math.sqrt((curLat*curLat) + (curLong*curLong)));
+
+      console.log(distance);
+
+      var color;
+
+      //Too far, color is red
+      if(distance > noiseMax){
+        color = "#FF0000";
+      }else{ //Range from red to green
+        var difference = noiseMax - distance;
+
+        console.log("Difference : "+difference);
+
+        //Get the change of color from red to green
+        var colorChange = Math.round(difference/noiseMax * 512);
+
+        console.log("colorChange: "+colorChange);
+
+        var green = colorChange < 256 ? numToHex(colorChange) : "FF";
+        var red = colorChange > 256 ? numToHex(512-colorChange) : "00";
+
+        color = "#"+red+green+"00";
+
+      }
+
+      return color;
+
+    }
+
+    function numToHex(c) {
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+    }
 
   }
 
